867df607c44856fc6abd587389dc0b48699573f1
rg <rgillespie@gmail.com>
2022-03-08 20:27:35 +0000

    jenkins-webhook

--- day3/interface_updater.py
+++ day3/interface_updater.py
@@ -1,3 +1,7 @@
+import os
+
+PAYLOAD = os.environ["PAYLOAD"]
+
 import json
 from scrapli import Scrapli
 
@@ -8,9 +12,15 @@ DC_DEVICE_PLATFORM_MAP = {
     "spine1": "arista_eos",
 }
 
+HOST_NAT_MAP = {
+    "leaf1": 2221,
+    "leaf2": 2222,
+    "leaf3": 2223,
+    "spine1": 2211,
+}
+
 def main():
-    with open("payload.json") as f:
-        stuff = json.load(f)
+    stuff = json.loads(PAYLOAD)
 
     device = stuff["data"]["device"]["name"]
     intf = stuff["data"]["name"]
@@ -18,7 +28,7 @@ def main():
 
     print(f"device {device} interface {intf} description now {new_descr}")
 
-    with Scrapli(host=device,auth_username="admin",auth_password="admin",auth_strict_key=False,platform=DC_DEVICE_PLATFORM_MAP[device]) as  conn:
+    with Scrapli(host=device,port=HOST_NAP_MAP[device],transport="paramiko",auth_username="admin",auth_password="admin",auth_strict_key=False,platform=DC_DEVICE_PLATFORM_MAP[device]) as  conn:
         current_descr = conn.send_command(command=f"show run interface {intf} | inc descr")
         if current_descr.result.strip() == new_descr:
             print("currnt descr matches, nothing to do")
